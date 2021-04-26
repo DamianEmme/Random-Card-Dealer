2ac637653e8e9ab46a53b15a751e0e168881dc77
DamianEmme <ademmenegger@gmail.com>
2021-04-26 13:41:20 +0000

    ejercicio cartas

--- src/app.js
+++ src/app.js
@@ -9,3 +9,46 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+window.onload = function() {
+  let figure = ["♠", "♣", "♥", "♦"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let figureIndex = Math.floor(Math.random() * figure.length);
+  let numberIndex = Math.floor(Math.random() * number.length);
+
+  {
+    document.querySelector("#palo").innerHTML += `<h1>
+            ${figure[figureIndex]}</h1>`;
+    document.querySelector("#numero").innerHTML += `<h1>
+            ${number[numberIndex]}</h1>`;
+    console.log(figure[figureIndex]);
+    document.querySelector("#paloB").innerHTML += `<h1>
+            ${figure[figureIndex]}</h1>`;
+  }
+  if (figure[figureIndex] == "♦" || figure[figureIndex] == "♥") {
+    document.getElementById("palo").style.color = "red";
+    document.getElementById("numero").style.color = "red";
+    document.getElementById("paloB").style.color = "red";
+  }
+};
+
+/*window.onload = () => {
+  document.querySelector(".container").addEventListener("click", () => {
+    document.querySelector("#newCard").innerHTML = generateCard();
+  });
+};*/
